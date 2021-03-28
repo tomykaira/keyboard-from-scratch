889a4e94c7fac42c28f11992dc65b1cdd3f155bc
whoami <whoami@example.com>
2021-03-29 01:53:44 +0900

    Change to STM32L412

--- firmware/src/peer.rs
+++ firmware/src/peer.rs
@@ -1,24 +1,24 @@
-use stm32f1xx_hal::gpio::gpiob::{PB6, PB7};
-use stm32f1xx_hal::gpio::{Alternate, OpenDrain};
-use stm32f1xx_hal::i2c::{BlockingI2c, Error};
-use stm32f1xx_hal::pac::I2C1;
-use stm32f1xx_hal::prelude::*;
+use stm32l4xx_hal::gpio::gpioa::{PA10, PA9};
+use stm32l4xx_hal::gpio::{Alternate, OpenDrain, Output, AF4};
+use stm32l4xx_hal::i2c::{Error, I2c};
+use stm32l4xx_hal::pac::I2C1;
+use stm32l4xx_hal::prelude::*;
 
 const ROWS_PER_HAND: u8 = 4;
 const SERIAL_SUB_BUFFER_LENGTH: u8 = 5;
 // const I2C_ADDRESS: u8 = 0x32u8;
 const I2C_ADDRESS: u8 = 0x19u8;
+type SclPin = PA9<Alternate<AF4, Output<OpenDrain>>>;
+type SdaPin = PA10<Alternate<AF4, Output<OpenDrain>>>;
 
 pub struct Peer {
-    i2c: BlockingI2c<I2C1, (PB6<Alternate<OpenDrain>>, PB7<Alternate<OpenDrain>>)>,
+    i2c: I2c<I2C1, (SclPin, SdaPin)>,
     pub serial_sub_buffer: [u8; SERIAL_SUB_BUFFER_LENGTH as usize],
-    pub error: Option<nb::Error<Error>>,
+    pub error: Option<Error>,
 }
 
 impl Peer {
-    pub fn new(
-        i2c: BlockingI2c<I2C1, (PB6<Alternate<OpenDrain>>, PB7<Alternate<OpenDrain>>)>,
-    ) -> Peer {
+    pub fn new(i2c: I2c<I2C1, (SclPin, SdaPin)>) -> Peer {
         return Peer {
             i2c,
             serial_sub_buffer: [0u8; SERIAL_SUB_BUFFER_LENGTH as usize],
